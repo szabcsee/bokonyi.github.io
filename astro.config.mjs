3033600986dd90770aac06aaa4a0b305ca46fc7d
Szabolcs Bokonyi <szabcsee@gmail.com>
2025-08-28 20:09:48 +0200

    Build refinement

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import { spectreDark } from './src/ec-theme';
 const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-// the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://bokonyi.github.io";
+// the url to access your blog after deploying it somewhere (Eg. GitHub Pages)
+const LIVE_URL = "https://bokonyi.com";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -52,8 +52,6 @@ export default defineConfig({
     }),
     mdx(),
   ],
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  
+  output: 'static',
 });
